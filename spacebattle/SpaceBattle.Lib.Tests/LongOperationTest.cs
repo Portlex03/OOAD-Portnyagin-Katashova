929d1e139a49e3353867d24307e2b7f1fc93ab61
Portlex <portnyagin924@gmail.com>
2023-12-14 22:20:55 +0600

    Исправлено форматирование и удалён лишний using

--- spacebattle/SpaceBattle.Lib.Tests/LongOperationTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/LongOperationTest.cs
@@ -2,7 +2,6 @@ namespace SpaceBattle.Lib.Tests;
 using Hwdtech.Ioc;
 using Hwdtech;
 using Moq;
-using Xunit.Sdk;
 
 public class LongOperationTest
 {
@@ -11,32 +10,32 @@ public class LongOperationTest
         new InitScopeBasedIoCImplementationCommand().Execute();
 
         IoC.Resolve<ICommand>(
-            "Scopes.Current.Set", 
-            IoC.Resolve<object>("Scopes.New", 
+            "Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New",
             IoC.Resolve<object>("Scopes.Root"))
         ).Execute();
 
         var stringsList = new Mock<IEnumerable<string>>();
         IoC.Resolve<ICommand>(
-            "IoC.Register","SetupStringOperation.NameOfDependence",
+            "IoC.Register", "SetupStringOperation.NameOfDependence",
             (object[] args) => stringsList.Object
         ).Execute();
 
         var macroCommand = new Mock<ICommand>();
         IoC.Resolve<ICommand>(
-            "IoC.Register","MacroCommand.Create",
+            "IoC.Register", "MacroCommand.Create",
             (object[] args) => macroCommand.Object
         ).Execute();
     }
-    
+
     [Fact]
     public void LongOperationCreatesWithoutErrors()
     {
         string cmdName = "NameOfDependence";
-        
+
         var target = new Mock<IUObject>();
 
-        var longOperation = new LongOperation(cmdName,target.Object);
+        var longOperation = new LongOperation(cmdName, target.Object);
         longOperation.Execute();
     }
 }
