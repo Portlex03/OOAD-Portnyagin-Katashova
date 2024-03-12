7fce5de4f9cdeae9dc5d61b280f7e46034227cc3
Portlex <portnyagin924@gmail.com>
2024-03-12 11:56:26 +0600

    Тест на GetHashCode

--- spacebattle/SpaceBattle.Lib.Tests/6_StatefulServer/ServerEqualsTest.cs
+++ spacebattle/SpaceBattle.Lib.Tests/6_StatefulServer/ServerEqualsTest.cs
@@ -7,24 +7,36 @@ public class ServerThreadEqualsTest
     [Fact]
     public void Object_Is_Null_Test()
     {
-        ServerThread? st = null;
+        ServerThread? st1 = null;
 
         var q = new BlockingCollection<ICommand>(10);
 
         var st2 = new ServerThread(q);
 
-        Assert.False(st2.Equals(st));
+        Assert.False(st2.Equals(st1));
     }
 
     [Fact]
     public void Object_Is_Not_ServerThread_Type_Test()
     {
-        int st = 1;
+        int st1 = 1;
 
         var q = new BlockingCollection<ICommand>(10);
 
         var st2 = new ServerThread(q);
 
-        Assert.False(st2.Equals(st));
+        Assert.False(st2.Equals(st1));
+    }
+
+    [Fact]
+    public void Two_different_objects_have_different_GetHashCodes()
+    {
+        var q = new BlockingCollection<ICommand>(10);
+
+        var st1 = new ServerThread(q);
+
+        var st2 = new ServerThread(q);
+
+        Assert.False(st1.GetHashCode() == st2.GetHashCode());
     }
 }
