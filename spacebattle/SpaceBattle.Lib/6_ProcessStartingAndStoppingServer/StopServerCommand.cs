d1df51bec1b7dd5d8502d58cb1b91fd1e131266f
Graf_Tonyakula <tonyakat87@gmail.com>
2024-03-24 22:28:50 +0600

    Произошло зависание теста

--- spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StopServerCommand.cs
+++ spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StopServerCommand.cs
@@ -6,6 +6,7 @@ public class StopServerCommand : ICommand
     public void Execute()
     {
         var threadDict = IoC.Resolve<Dictionary<int, object>>("Server.GetThreadDict");
+        Barrier barrier = new Barrier(threadDict.Count + 1); // Создаем Barrier с количеством потоков
 
         // каждому элементу словаря посылаем с помощью
         // отправителя команду остановки
@@ -14,9 +15,11 @@ public class StopServerCommand : ICommand
                 "Server.Thread.SendCommand",
                 idAndThreadPair.Key,
                 IoC.Resolve<ICommand>(
-                    "Server.Thread.SoftStop", idAndThreadPair.Key
-                )
+                    "Server.Thread.SoftStop", idAndThreadPair.Key,
+                    () => { barrier.SignalAndWait(); })
             ).Execute()
         );
+
+        barrier.SignalAndWait(); // Ждем завершения всех потоков
     }
 }
