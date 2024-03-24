7b1c94f7663130fd311cae85b696dee68d85c9e2
Graf_Tonyakula <tonyakat87@gmail.com>
2024-03-24 19:00:23 +0600

    Delete cycle "for -> range"

--- spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StartServerCommand.cs
+++ spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StartServerCommand.cs
@@ -7,9 +7,12 @@ public class StartServerCommand : ICommand
     public StartServerCommand(int threadCount) => _threadCount = threadCount;
     public void Execute()
     {
-        for (int threadId = 0; threadId < _threadCount; threadId++)
-        {
-            IoC.Resolve<ICommand>("Server.Thread.Create&Start", threadId).Execute();
-        }
+        Enumerable.Range(0, _threadCount).ToList().ForEach(threadId =>
+            IoC.Resolve<ICommand>("Server.Thread.Create&Start", threadId
+        ).Execute());
+        // for (int threadId = 0; threadId < _threadCount; threadId++)
+        // {
+        //     IoC.Resolve<ICommand>("Server.Thread.Create&Start", threadId).Execute();
+        // }
     }
 }
