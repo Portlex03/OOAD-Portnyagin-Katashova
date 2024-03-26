c40f932b91c83e145d383d09b120de5a2d67394d
Graf_Tonyakula <tonyakat87@gmail.com>
2024-03-26 19:22:14 +0600

    temporarily

--- spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StopServerCommand.cs
+++ spacebattle/SpaceBattle.Lib/6_ProcessStartingAndStoppingServer/StopServerCommand.cs
@@ -6,7 +6,7 @@ public class StopServerCommand : ICommand
     public void Execute()
     {
         var threadDict = IoC.Resolve<Dictionary<int, object>>("Server.GetThreadDict");
-        Barrier barrier = new Barrier(threadDict.Count + 1); // Создаем Barrier с количеством потоков
+        Barrier barrier = new Barrier(threadDict.Count); // Создаем Barrier с количеством потоков
 
         // каждому элементу словаря посылаем с помощью
         // отправителя команду остановки
@@ -20,6 +20,6 @@ public class StopServerCommand : ICommand
             ).Execute()
         );
 
-        barrier.SignalAndWait(); // Ждем завершения всех потоков
+        // barrier.SignalAndWait(); // Ждем завершения всех потоков
     }
 }
