a2ad2d8a46fc08ec11330a754c9a068c5a6c1f82
Graf_Tonyakula <tonyakat87@gmail.com>
2024-04-23 18:16:44 +0600

    Редактирование файла

--- spacebattle/SpaceBattle.Lib/7_MessageProcessing/GetObject.cs
+++ spacebattle/SpaceBattle.Lib/7_MessageProcessing/GetObject.cs
@@ -1,11 +1,20 @@
 namespace SpaceBattle.Lib;
+
 using Hwdtech;
 
 public class GetObject : IStrategy
 {
+    private readonly IUObject message;
+    public GetObject(IUObject obj)
+    {
+        message = obj;
+    }
     public object Execute(params object[] args)
     {
-        IoC.Resolve<Queue<IUObject>>("SpaceBattle.Queue").TryDequeue(out IUObject? obj); // вопрос исключает возможность null?
-        return obj ?? throw new Exception();
+        IoC.Resolve<Dictionary<string, IUObject>>("SpaceBattle.GetMessage").TryGetValue((string)args[0], out IUObject? message);
+
+        IoC.Resolve<Queue<IUObject>>("SpaceBattle.Queue").TryDequeue(out message);
+
+        return message ?? throw new Exception();
     }
 }
