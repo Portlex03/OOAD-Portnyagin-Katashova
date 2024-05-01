13b15325e3e523efe2f043dd59a2e09cf0bf4fe2
Portlex <portnyagin924@gmail.com>
2024-05-01 12:38:45 +0600

    Remove starting stopwatch in cycle and remove creating stopwatch in constructor

--- spacebattle/SpaceBattle.Lib/8_GameAsCommandLab/GameAsCommand.cs
+++ spacebattle/SpaceBattle.Lib/8_GameAsCommandLab/GameAsCommand.cs
@@ -1,27 +1,27 @@
-namespace SpaceBattle.Lib;
 using Hwdtech;
 using System.Diagnostics;
+
+namespace SpaceBattle.Lib;
 
 public class GameAsCommand : ICommand
 {
     readonly object _scope;
     readonly Queue<ICommand> _q;
+    readonly Stopwatch _stopwatch;
 
     public GameAsCommand(object scope, Queue<ICommand> q)
     {
         _scope = scope;
         _q = q;
+        _stopwatch = new();
     }
 
     public void Execute()
     {
         IoC.Resolve<ICommand>("Scopes.Current.Set", _scope).Execute();
-
-        var stopWatch = new Stopwatch();
-
-        stopWatch.Start();
-        while (stopWatch.ElapsedMilliseconds <= IoC.Resolve<int>("Game.Quant"))
+        while (_stopwatch.ElapsedMilliseconds <= IoC.Resolve<int>("Game.Quant"))
         {
+            _stopwatch.Start();
             if (_q.Count == 0)
                 break;
             var cmd = _q.Dequeue();
@@ -33,7 +33,8 @@ public class GameAsCommand : ICommand
             {
                 IoC.Resolve<ICommand>("Exception.Handler", ex, cmd).Execute();
             }
+            _stopwatch.Stop();
         }
-        stopWatch.Stop();
+        _stopwatch.Reset();
     }
 }
