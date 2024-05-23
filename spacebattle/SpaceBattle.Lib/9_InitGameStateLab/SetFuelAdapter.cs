54443568783cbd279a36dff37e6843353244dc4a
Portlex <portnyagin924@gmail.com>
2024-05-23 18:46:16 +0600

    feat: create SetFuelAdapter

---
+++ spacebattle/SpaceBattle.Lib/9_InitGameStateLab/SetFuelAdapter.cs
@@ -0,0 +1,13 @@
+namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class SetFuelAdapter : IFuelBurnable
+{
+    private readonly IUObject _uobject;
+    public SetFuelAdapter(IUObject uobject) => _uobject = uobject;
+
+    public double FuelVolume
+    {
+        set => IoC.Resolve<ICommand>("UObject.SetProperty", "FuelVolume", _uobject, value).Execute();
+    }
+}
